2e1a3289e67a67011bb0204e0daa497b6b995d6f
kennytram <kennyvtram@gmail.com>
2024-08-20 09:15:45 -0700

    Update names

--- w.rb
+++ w.rb
@@ -51,8 +51,8 @@ File.open(w_file, 'r') do |file|
         line_num += 1
         if line_num >= start_data_table_row && line_num < end_data_table_row
             data = line.strip.split
-            santitize_w_data(data, HDDay_col_num, HRP_col_num, WxType_col_num)
-            santitize_cols(data, max_temp_col, min_temp_col, mxs_col_num)
+            sanitize_w_data(data, HDDay_col_num, HRP_col_num, WxType_col_num)
+            sanitize_cols(data, max_temp_col, min_temp_col, mxs_col_num)
             max_temp = data[max_temp_col].tr('^0-9.', '').to_i
             min_temp = data[min_temp_col].tr('^0-9.', '').to_i
             diff = (max_temp - min_temp).abs
